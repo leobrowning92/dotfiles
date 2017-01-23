d83e68b866bdad273c0f7a7a9c499a5208d3c54b
Leo Browning <leobrowning92@gmail.com>
2017-01-23 13:29:52 +1300

    updated some of the arch files for a clean install. also commented out apm commands for latex

--- atom-install.sh
+++ atom-install.sh
@@ -3,6 +3,6 @@ apm install file-icons  minimap script autocomplete-paths
 #atom packages for using git
 apm install git-plus markdown-pdf
 #atom packages for latex functionality
-apm install latex language-latex pdf-view
+#apm install latex language-latex pdf-view
 #atom packages for python functionality
 apm install python-tools autocomplete-python
