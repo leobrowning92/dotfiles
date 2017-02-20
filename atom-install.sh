0f851b39c0c846f08ba22841850a1ee559ffc483
leobrowning92 <leobrowning92@gmail.com>
2017-02-20 16:51:24 +1300

    made my atom install script more explicit

--- atom-install.sh
+++ atom-install.sh
@@ -1,8 +1,11 @@
 #add atom packages for minimum functionality
+#these are good regardless of what you are going to do
 apm install file-icons  minimap script autocomplete-paths
 #atom packages for using git
-apm install git-plus markdown-pdf
+apm install git-plus
+#just for markdown if you use it
+apm install markdown-pdf
 #atom packages for latex functionality
-#apm install latex language-latex pdf-view
+apm install latex language-latex pdf-view
 #atom packages for python functionality
 apm install python-tools autocomplete-python
